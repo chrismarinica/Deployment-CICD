7fca182cadc46147e91f48824a1e9c055e015b61
Christopher Marinica <chris.marinica@gmail.com>
2025-04-27 15:49:33 -0600

    fixed seed process

--- Develop/server/src/seeds/seed.ts
+++ Develop/server/src/seeds/seed.ts
@@ -1,14 +1,17 @@
+import fs from 'fs';
+import path from 'path';
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: 'json' };
+// Get the current file path and resolve the json file
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+const pythonQuestions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../seeds/pythonQuestions.json'), 'utf8'));
 
 db.once('open', async () => {
-  await cleanDB('Question', 'questions');
-
-  await Question.insertMany(pythonQuestions);
-
-  console.log('Questions seeded!');
-  process.exit(0);
+    await cleanDB('Question', 'questions');
+    await Question.insertMany(pythonQuestions);
+    console.log('Questions seeded!');
+    process.exit(0);
 });
