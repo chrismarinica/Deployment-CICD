24faf7c731935fe1d881fdb79e7204128f3e9e0f
Christopher Marinica <chris.marinica@gmail.com>
2025-04-28 18:38:57 -0600

    fix: connect using MONGO_URI

--- Develop/server/src/config/connection.ts
+++ Develop/server/src/config/connection.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/codequiz')
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 export default mongoose.connection;
